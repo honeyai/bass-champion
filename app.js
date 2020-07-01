5f28eb6ffc9e2ec6d24c3d43550c359c5ec3a583
Victoria Jones <victoriajones@psadmins-air-3.lan>
2020-06-30 20:01:48 -0400

    added songs object and accuracy/pts

--- app.js
+++ app.js
@@ -1,2 +1,41 @@
 console.log("Working")
 
+
+
+
+
+
+// This is an object to hold the songs players can choose from.
+let songs = {
+    options : [
+    {title: "Let's Groove",
+     artist: "Earth, Wind, and Fire",
+     difficulty: 3,
+    cover: ""},
+     {title: "Knights of Cydonia",
+    artist: "Muse",
+    difficulty: 5,
+    cover: ""},
+    {title: "Bat Country",
+    artist: "Avenged Sevenfold",
+    difficulty: 4,
+    cover: ""},
+    {title: "Decode",
+    artist: "Paramore",
+    difficulty: 2,
+    cover: ""},
+    {title: "Feels Like We Only Go Backwards",
+    artist: "Tame Impala",
+    difficulty: 1,
+    cover: ""}
+    ]
+}
+
+
+// Counters for player and computer accuracy and points
+let playerAccuracy = 100;
+let playerPoints = 0;
+
+let compAccuracy = 100;
+let compPoints = 0;
+
